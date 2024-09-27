fd4fb287ad2f20d3f4b10d01d75bf9f99df40ad9
seong2808 <108581844+seong2808@users.noreply.github.com>
2024-09-28 01:33:29 +0900

    [Bronze V] Title: 시험 성적, Time: 100 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(br.readLine());
+
+        int score = Integer.parseInt(tk.nextToken());
+
+        switch (score/10) {
+            case 10:
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+                break;
+
+        }
+    }
+}
