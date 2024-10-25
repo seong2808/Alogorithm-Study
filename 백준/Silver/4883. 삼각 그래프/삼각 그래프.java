0084b506e815e100d338b828d8c6df79b02f8626
seong2808 <108581844+seong2808@users.noreply.github.com>
2024-10-25 19:25:09 +0900

    [Silver I] Title: 삼각 그래프, Time: 396 ms, Memory: 49828 KB -BaekjoonHub

--- 백준/Silver/4883. 삼각 그래프/삼각 그래프.java
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.java
@@ -1,6 +1,7 @@
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
  
 public class Main {
 	public static int N, M, K;
