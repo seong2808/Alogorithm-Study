fc3daad6d5b0426f165535d301c6bdab78117287
seong2808 <108581844+seong2808@users.noreply.github.com>
2024-11-16 22:35:12 +0900

    [Bronze IV] Title: 기찍 N, Time: 852 ms, Memory: 31772 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(tk.nextToken());
+
+
+        for (int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+
+    }
+}
+
+
