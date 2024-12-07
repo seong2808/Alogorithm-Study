8712ea86f7012d8ed96338bc200eb33244d67344
seong2808 <108581844+seong2808@users.noreply.github.com>
2024-12-07 21:19:54 +0900

    [Silver IV] Title: 덱, Time: 168 ms, Memory: 18508 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer tk;
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            tk = new StringTokenizer(br.readLine());
+
+            switch (tk.nextToken()) {
+                case "push_front" :
+                    list.add(0, Integer.parseInt(tk.nextToken()));
+                    break;
+                case "push_back" :
+                    list.add(Integer.parseInt(tk.nextToken()));
+                    break;
+                case "pop_front" :
+                    if (list.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(list.get(0)).append("\n");
+                        list.remove(0);
+                    }
+                    break;
+                case "pop_back" :
+                    if (list.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(list.get(list.size()-1)).append("\n");
+                        list.remove(list.size()-1);
+                    }
+                    break;
+                case "size" :
+                    sb.append(list.size()).append("\n");
+                    break;
+                case "empty" :
+                    if (list.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "front" :
+                    if (list.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(list.get(0)).append("\n");
+                    }
+                    break;
+                case "back" :
+                    if (list.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(list.get(list.size() - 1)).append("\n");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
