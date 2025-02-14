262cc811588f7433a4bcca0cf1721b0b36dbb1d9
seong2808 <108581844+seong2808@users.noreply.github.com>
2025-02-14 17:13:04 +0900

    [Silver III] Title: N과 M (5), Time: 308 ms, Memory: 29240 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[] result, seq;
+    private static boolean[] visited;
+    private static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tk = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(tk.nextToken());
+        visited = new boolean[N];
+        seq = new int[N];
+        M = Integer.parseInt(tk.nextToken());
+        result = new int[M];
+
+        tk = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            seq[i] = Integer.parseInt(tk.nextToken());
+        }
+        Arrays.sort(seq);
+
+
+        bt(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void bt(int index, int dept) {
+        if (dept == M) {
+            for (int val : result)
+                sb.append(val).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[dept] = seq[i];
+                bt(i, dept + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
